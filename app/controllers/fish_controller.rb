69ea5308c49f3d11156894033cbd495e70259bbb
Robert Cook <90288134+bobobnj@users.noreply.github.com>
2021-11-02 10:05:26 -0700

    Read/Create/Update complete

--- app/controllers/fish_controller.rb
+++ app/controllers/fish_controller.rb
@@ -1,2 +1,27 @@
 class FishController < ApplicationController
+
+    def index
+        fish = Fish.all
+        render json: fish
+    end
+    def create
+        fish = Fish.create(fish_params)
+        render json: fish
+    end
+    def update
+        fish = Fish.find(params[:id])
+        fish.update(fish_params)
+        render json: fish
+    end
+    def destroy
+        fish = Fish.find(params[:id])
+        fish.destroy
+        render json: fish
+      end
+
+    private
+    def fish_params
+        params.require(:fish).permit(:name, :age, :enjoys)
+    end
 end
+
