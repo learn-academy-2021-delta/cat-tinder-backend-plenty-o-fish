ace1cca45b4133a68aa7f39b0ceeeb0982274b76
Robert Cook <90288134+bobobnj@users.noreply.github.com>
2021-11-03 12:03:18 -0700

    modelspecs validation/request validation

--- app/controllers/fish_controller.rb
+++ app/controllers/fish_controller.rb
@@ -1,27 +1,38 @@
 class FishController < ApplicationController
-
+#Anything to do with the controller is a spec/request
     def index
         fish = Fish.all
         render json: fish
     end
+
     def create
         fish = Fish.create(fish_params)
-        render json: fish
+        if fish.valid?
+            render json: fish
+        else 
+            render json: fish.errors, status: 422
+        end
     end
+
     def update
         fish = Fish.find(params[:id])
         fish.update(fish_params)
-        render json: fish
+        if fish.valid?
+          render json: fish
+        else 
+          render json: fish.errors, status: 422
     end
+  end
+
     def destroy
         fish = Fish.find(params[:id])
         fish.destroy
         render json: fish
-      end
-
-    private
-    def fish_params
-        params.require(:fish).permit(:name, :age, :enjoys)
     end
-end
-
+    
+      private
+      def fish_params
+        params.require(:fish).permit(:name, :age, :enjoys)
+      end
+    
+ end
--- spec/requests/fish_spec.rb
+++ spec/requests/fish_spec.rb
@@ -2,23 +2,23 @@ require 'rails_helper'
 
 RSpec.describe "Fish", type: :request do
   describe "GET /index" do
-    it "gets a list of fish" do
+    it 'gets a list of fish' do
     Fish.create name:'Sparky', age: 5, enjoys: 'licking light sockets'
     get '/fish'
     fish = JSON.parse(response.body)
     expect(response).to have_http_status(200)
     expect(fish.length).to eq 1
-   end
+    end
   end
 
   describe "POST /create" do
-    it "will create a fish" do
+    it 'will create a fish' do
 
       fish_params = {
         fish: {
           name: 'Scuffle',
           age: 1,
-          enjoys: 'Pinching'
+          enjoys: 'Pinching everything'
         }
       }
 
@@ -29,12 +29,12 @@ RSpec.describe "Fish", type: :request do
       p fish.id
       expect(fish.name).to eq 'Scuffle'
       expect(fish.age).to eq 1
-      expect(fish.enjoys).to eq 'Pinching'
+      expect(fish.enjoys).to eq 'Pinching everything'
     end
   end
 
   describe "PATCH /update" do
-    it "updates a fish" do
+    it 'updates a fish' do
       fish_params = {
         fish: {
           name: 'Diana',
@@ -47,7 +47,7 @@ RSpec.describe "Fish", type: :request do
 
       updated_fish_params = {
         fish: {
-          name: "Diana",
+          name: 'Diana',
           age: 33,
           enjoys: 'Getting dirty'
         }
@@ -58,4 +58,151 @@ RSpec.describe "Fish", type: :request do
       expect(fish.age).to eq 33
     end
   end
-end   
+    describe "DELETE /destroy" do
+      it 'deletes a fish' do
+        fish_params = {
+          fish: {
+            name: 'Snooky',
+            age: 44,
+            enjoys: 'Raging on the Jersey Shore' 
+          }
+        }
+            post '/fish', params: fish_params
+          fish = Fish.first
+          delete "/fish/#{fish.id}"
+          expect(response).to have_http_status(200)
+          fish = Fish.all
+          expect(fish).to be_empty
+          end
+        end
+
+        describe 'fish validation error codes' do
+          it 'does not create a fish with a name' do
+            fish_params = {#leave name out to test name
+              fish: {
+                age: 2,
+                enjoys: 'Raging on the Jersey Shore'
+              }
+            }
+            post '/fish', params: fish_params
+            expect(response).to have_http_status(422)
+            fish = JSON.parse(response.body)
+            expect(fish['name']).to include "can't be blank"
+          end
+          it 'does not create a fish with an age' do
+            fish_params = {#leave age out to test age
+              fish: {
+                name: 'Snooky',
+                enjoys: 'Raging on the Jersey Shore'
+              }
+            }
+            post '/fish', params: fish_params
+            expect(response).to have_http_status(422)
+            fish = JSON.parse(response.body)
+            expect(fish['age']).to include "can't be blank"
+          end
+          it 'does not create a fish with an enjoys' do
+            fish_params = {#leave enjoys out to test enjoys
+              fish: {
+                name: 'Snooky',
+                age: 3
+              }
+            }
+            post '/fish', params: fish_params
+            expect(response).to have_http_status(422)
+            fish = JSON.parse(response.body)
+            expect(fish['enjoys']).to include "can't be blank"
+          end
+        end
+      describe "cannot update a fish without valid attributes" do
+          it 'cannot update a fish without a name' do
+            fish_params = {
+              fish: {
+                name: 'Nala',
+                age: 2,
+                enjoys: 'Big squuezes'
+              }
+            }
+            post '/fish', params: fish_params
+            fish = Fish.first
+            fish_params = {
+              fish: {
+                name: '',
+                age: 2,
+                enjoys: 'Big squuezes'
+              }
+            }
+            patch "/fish/#{fish.id}", params: fish_params
+            fish = JSON.parse(response.body)
+            expect(response).to have_http_status(422)
+            expect(fish['name']).to include "can't be blank"
+          end
+          it 'cannot update a fish without a age' do
+            fish_params = {
+              fish: {
+                name: 'Nala',
+                age: 4,
+                enjoys: 'Tight squeezes'
+              }
+            }
+            post '/fish', params: fish_params
+            fish = Fish.first
+            fish_params = {
+              fish: {
+                name: 'Nala',
+                age: '',
+                enjoys: 'Big squuezes'
+              }
+            }
+            patch "/fish/#{fish.id}", params: fish_params
+            fish = JSON.parse(response.body)
+            expect(response).to have_http_status(422)
+            expect(fish['age']).to include "can't be blank"
+          end
+          it 'cannot update a fish without an enjoys' do
+            fish_params = {
+              fish: {
+                name: 'Nala',
+                age: 4,
+                enjoys: 'Big squuezes'
+              }
+            }
+            post '/fish', params: fish_params
+            fish = Fish.first
+            fish_params = {
+              fish: {
+                name: 'Nala',
+                age: 4,
+                enjoys: '',
+              }
+            }
+            patch "/fish/#{fish.id}", params: fish_params
+            fish = JSON.parse(response.body)
+            expect(response).to have_http_status(422)
+            expect(fish['enjoys']).to include "can't be blank"
+          end
+          it 'cannot update a fish without an enjoys that is at least 10 characters' do
+            fish_params = {
+              fish: {
+                name: 'Nala',
+                age: 4,
+                enjoys: 'Big squuezes'
+              }
+            }
+            post '/fish', params: fish_params
+            fish = Fish.first
+            fish_params = {
+              fish: {
+                name: 'Nala',
+                age: 4,
+                enjoys: 'cuddles'
+              }
+            }
+            patch "/fish/#{fish.id}", params: fish_params
+            fish = JSON.parse(response.body)
+            expect(response).to have_http_status(422)
+            expect(fish['enjoys']).to include 'is too short (minimum is 10 characters)'
+          end
+        end
+      end          
+       
